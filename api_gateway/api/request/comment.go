b0e7e85aa73427d1cdf9b5bd707e4cb085b5291d
zqy14 <3285569480@qq.com>
2025-07-23 21:46:17 +0800

    Delete api_gateway directory

--- api_gateway/api/request/comment.go
+++
@@ -1,9 +0,0 @@
-package request
-
-type PostComment struct {
-	WorkId  int64  `form:"work_id" binding:"required"`
-	UserId  int64  `form:"user_id" binding:"required"`
-	Content string `form:"content"`
-	Tag     int64  `form:"tag" binding:"required"`
-	Pid     int64  `form:"pid" binding:"required"`
-}
